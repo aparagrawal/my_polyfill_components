e2bbe2ff4798d3c30ca902573ad406005649e150
Apar <aparagrawal7@gmail.com>
2025-06-15 12:11:18 +0530

    slice wit array

--- Polyfills/slicepolyfill.js
+++ Polyfills/slicepolyfill.js
@@ -1,4 +1,4 @@
-// slice polyfill
+// slice polyfill with string
 
 String.prototype.mySlice = function (start = 0, end = this.length) {
 	const str = this.toString(); // make sure it's a primitive string
@@ -19,3 +19,21 @@ const word = "JavaScript";
 
 console.log(word.mySlice(0, 4));
 console.log(word.mySlice(-6, -3));
+
+// slice() polyfill with array
+
+Array.prototype.mySlicewithArray = function (start = 0, end = this.length) {
+	const result = [];
+	const len = this.length;
+
+	let from = start < 0 ? Math.max(len + start, 0) : Math.min(start, len);
+	let to = end < 0 ? Math.max(len + end, 0) : Math.min(end, len);
+
+	for (let i = from; i < to; i++) {
+		result.push(this[i]);
+	}
+
+	return result;
+};
+
+console.log([10, 20, 30, 40].mySlicewithArray(1, 3));
